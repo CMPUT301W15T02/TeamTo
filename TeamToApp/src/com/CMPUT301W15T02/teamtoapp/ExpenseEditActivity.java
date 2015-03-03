d8710b9c7d72f7103da9fddd258f4c6a33cad839
Kyle Carlstrom <kcarlstr@ualberta.ca>
2015-03-02 20:24:19 -0700

    Refactored to better line up with UML

--- TeamToApp/src/com/CMPUT301W15T02/teamtoapp/ExpenseEditActivity.java
+++ TeamToApp/src/com/CMPUT301W15T02/teamtoapp/ExpenseEditActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class AddExpenseActivity extends Activity {
+public class ExpenseEditActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_expense);
+		setContentView(R.layout.expense_edit_activity);
 	}
 
 	@Override
