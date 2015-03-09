04716a80057b16b1b8ff3bc6a9d3ff433238fafa
ChristineShaffer <cshaffer@ualberta.ca>
2015-03-09 07:38:04 -0600

    Added functionality to testGetClaimName in ClaimTest

--- TeamToApp/src/com/CMPUT301W15T02/teamtoapp/Claim.java
+++ TeamToApp/src/com/CMPUT301W15T02/teamtoapp/Claim.java
@@ -228,5 +228,4 @@ public class Claim extends Observable {
 		return ClaimId;
 	}
 	
-	
 }
--- TeamToAppTestTest/src/com/CMPUT301W15T02/teamtoapp/test/ClaimTest.java
+++ TeamToAppTestTest/src/com/CMPUT301W15T02/teamtoapp/test/ClaimTest.java
@@ -15,6 +15,17 @@ public class ClaimTest extends TestCase {
 	}
 	
 	public void testGetClaimName() {
+		String defaultClaimName = "New Claim";
+		Claim claim = new Claim();
+		assertTrue("Default claim name not 'New Claim'", claim.getClaimName().equals(defaultClaimName));
 		
+		String newClaimName = "newTestClaim";
+		claim.setClaimName(newClaimName);
+		assertTrue("Claim name not equal", claim.getClaimName().equals(newClaimName));
+		
+		String aSecondClaimName = "updatedTestClaim";
+		claim.setClaimName(aSecondClaimName);
+		assertTrue("Claim name not updated", claim.getClaimName().equals(aSecondClaimName));
 	}
+	
 }
