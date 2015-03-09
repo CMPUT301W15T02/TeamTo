d54fade77d57f5757b5bab393205f6d8064fd714
Stensby <michael.stensby@gmail.com>
2015-03-09 15:32:05 -0600

    Added internet permissions and method to check for connection to server

--- TeamToApp/src/com/CMPUT301W15T02/teamtoapp/SessionController.java
+++ TeamToApp/src/com/CMPUT301W15T02/teamtoapp/SessionController.java
@@ -1,5 +1,7 @@
 package com.CMPUT301W15T02.teamtoapp;
 
+import java.net.InetAddress;
+
 
 public class SessionController {
 	
@@ -8,5 +10,20 @@ public class SessionController {
 	
 	public SessionController() {
 	}
-	
+
+	public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("cmput301.softwareprocess.es:8080/cmput301w15t02/");
+
+            if (ipAddr.equals("")) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
 }
