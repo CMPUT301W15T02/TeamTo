6abb61c2e219fdebc8371045c847b33ed45e7a40
ChristineShaffer <cshaffer@ualberta.ca>
2015-03-09 07:07:36 -0600

    Making test cases for each class in model

--- TeamToAppTestTest/src/ClaimTest.java
+++ TeamToAppTestTest/src/ClaimTest.java
@@ -1,7 +1,14 @@
+import junit.framework.TestCase;
+import com.CMPUT301W15T02.teamtoapp.Claim;
 
-public class ClaimTest {
+
+public class ClaimTest extends TestCase {
 
 	public void testAddClaim() {
-		
+		String claimName = "claim1";
+		Claim claim = new Claim();
+		assertTrue("Claim name is not equal", claim.getClaimName().equals(claimName));
 	}
+		
+
 }
