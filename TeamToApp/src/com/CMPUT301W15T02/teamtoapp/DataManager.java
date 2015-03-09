ea91f1edcfebdc540af068c35d2b4d0f8118150b
Stensby <michael.stensby@gmail.com>
2015-03-09 13:16:36 -0600

    Added DataManager skeleton code and network permissions

---
+++ TeamToApp/src/com/CMPUT301W15T02/teamtoapp/DataManager.java
@@ -0,0 +1,23 @@
+package com.CMPUT301W15T02.teamtoapp;
+
+public class DataManager {
+
+	
+	public User getUser(){
+		
+		return null;
+	}
+	
+	public void saveUser(){
+		
+	}
+	
+	public ClaimsList getClaims(){
+		
+		return null;
+	}
+	
+	public void saveClaims(ClaimsList claimsList){
+		
+	}
+}
